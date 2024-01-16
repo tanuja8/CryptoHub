e247c9ee2195db9201f8ff7e7869f12aa16d6214
Tanuja <tanujakhatun292@gmail.com>
2024-01-17 00:44:38 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { Button, Menu, Typography, Avatar } from 'antd';
+import { Link } from 'react-router-dom';
+import { HomeOutlined,MoneyCollectOutlined,BulbFilled,MenuOutlined } from '@ant-design/icons';
+
+
+
+const Navbar = () => {
+  return (
+    <div className='nav-container'>
+        <div className='logo-container'>
+            <Avatar/>
+        </div>
+      
+    </div>
+  )
+}
+
+export default Navbar
+ 

266710388a3991fe9576462bf90b1c7c76fa013f
Tanuja <tanujakhatun292@gmail.com>
2024-01-20 01:22:44 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import { Switch, Route } from 'antd'; 
-import { Link } from 'antd';
-import {Layout , Typography, Space } from 'antd'
-import {Navbar} from './components';
-import './App.css'
+import React from "react";
+import { Switch, Route } from "antd";
+import { Link } from "antd";
+import { Layout, Typography, Space } from "antd";
+import { Navbar } from "./components";
+import "./App.css";
+import { Routes } from "react-router-dom";
 
 const App = () => {
   return (
     <div className="app">
       <div className="navbar">
-        <Navbar/>
+        <Navbar />
       </div>
-      <div className='main'>
-
+      <div className="main">
+        <Layout>
+          <div className="routes">
+              <Switch>
+                <Routes>
+                  
+                </Routes>
+              </Switch>
+          </div>
+        </Layout>
       </div>
-      <div className='footer'>
-        
-      </div>
+      <div className="footer"></div>
     </div>
-  )
-}
+  );
+};
 
 export default App;

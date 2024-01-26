072d54df3673868882a416a050e1727fd3e24a75
Tanuja <tanujakhatun292@gmail.com>
2024-01-26 20:36:16 +0530

    project updated

--- src/App.js
+++ src/App.js
@@ -1,54 +1,107 @@
-import React from "react";
-// import { Switch } from "antd";
-import { Route, Switch } from "react-router-dom";
-import { Link } from "react-router-dom";
-import { Layout, Typography, Space } from "antd";
-import {
-  Navbar,
-  Exchanges,
-  Homepage,
-  Cryptocurrencies,
-  News,
-  CryptoDetail,
-} from "./components";
-import "./App.css";
+// import React from "react";
+// // import { Switch } from "antd";
+// import { Route, Switch,Link } from "react-router-dom";
+// import { Layout, Typography, Space } from "antd";
+// import {
+//   Navbar,
+//   Exchanges,
+//   Homepage,
+//   Cryptocurrencies,
+//   News,
+//   CryptoDetail,
+// } from "./components";
+// import "./App.css";
 
-const App = () => {
-  return (
-    <div className="app">
-      <div className="navbar">
-        <Navbar />
-      </div>
-      <div className="main">
-        <Layout>
-          <div className="routes">
-            <Switch>
-              <Route exact path="/">
-                <Homepage />
-              </Route>
-              <Route exact path="/abc">
-                <Exchanges />
-              </Route>
-              <Route exact path="/cryptocurrencies">
-                <Cryptocurrencies />
-              </Route>
-              <Route exact path="/Crypto/:coinId">
-                <CryptoDetail />
-              </Route>
-              <Route exact path="/news">
-                <News />
-              </Route>
-            </Switch>
-          </div>
-        </Layout>
-      </div>
+// const App = () => {
+//   return (
+//     <div className="app">
+//       <div className="navbar">
+//         <Navbar />
+//       </div>
+//       <div className="main">
+//         <Layout>
+//           <div className="routes">
+//             <Switch>
+//               <Route exact path="/">
+//                 <Homepage />
+//               </Route>
+//               <Route exact path="/abc">
+//                 <Exchanges />
+//               </Route>
+//               <Route exact path="/cryptocurrencies">
+//                 <Cryptocurrencies />
+//               </Route>
+//               <Route exact path="/Crypto/:coinId">
+//                 <CryptoDetail />
+//               </Route>
+//               <Route exact path="/news">
+//                 <News />
+//               </Route>
+//             </Switch>
+//           </div>
+//         </Layout>
+//       </div>
+//       <div className="footer">
+//         <Typography.Title
+//           level={5}
+//           style={{ color: "white", textAlign: "center" }}
+//         >
+//           CryptoHub <br />
+//           All rights reserved
+//         </Typography.Title>
+//         <Space>
+//           <Link to="/">Home</Link>
+//           <Link to="/exchanges">Exchanges</Link>
+//           <Link to="/news">News</Link>
+//         </Space>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
+
+import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetail, } from "./components";
+import './App.css';
+
+const App = () => (
+  <div className="app">
+    <div className="navbar">
+      <Navbar />
+    </div>
+    <div className="main">
+      <Layout>
+        <div className="routes">
+          <Switch>
+            <Route exact path="/">
+              <Homepage />
+            </Route>
+            <Route exact path="/exchanges">
+              <Exchanges />
+            </Route>
+            <Route exact path="/cryptocurrencies">
+              <Cryptocurrencies />
+            </Route>
+            <Route exact path="/crypto/:coinId">
+              <CryptoDetail/>
+            </Route>
+            <Route exact path="/news">
+              <News />
+            </Route>
+          </Switch>
+        </div>
+      </Layout>
       <div className="footer">
-        <Typography.Title
-          level={5}
-          style={{ color: "white", textAlign: "center" }}
-        >
-          CryptoHub <br />
-          All rights reserved
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+          <Link to="/">
+            Cryptoverse Inc.
+          </Link> <br />
+          All Rights Reserved.
         </Typography.Title>
         <Space>
           <Link to="/">Home</Link>
@@ -57,7 +110,7 @@ const App = () => {
         </Space>
       </div>
     </div>
-  );
-};
+  </div>
+);
 
 export default App;

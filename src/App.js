a4dbdcb19de62035493a29bdf26bb36ed699d104
Tanuja <tanujakhatun292@gmail.com>
2024-01-28 13:14:02 +0530

    homepage updated

--- src/App.js
+++ src/App.js
@@ -1,74 +1,9 @@
-// import React from "react";
-// // import { Switch } from "antd";
-// import { Route, Switch,Link } from "react-router-dom";
-// import { Layout, Typography, Space } from "antd";
-// import {
-//   Navbar,
-//   Exchanges,
-//   Homepage,
-//   Cryptocurrencies,
-//   News,
-//   CryptoDetail,
-// } from "./components";
-// import "./App.css";
-
-// const App = () => {
-//   return (
-//     <div className="app">
-//       <div className="navbar">
-//         <Navbar />
-//       </div>
-//       <div className="main">
-//         <Layout>
-//           <div className="routes">
-//             <Switch>
-//               <Route exact path="/">
-//                 <Homepage />
-//               </Route>
-//               <Route exact path="/abc">
-//                 <Exchanges />
-//               </Route>
-//               <Route exact path="/cryptocurrencies">
-//                 <Cryptocurrencies />
-//               </Route>
-//               <Route exact path="/Crypto/:coinId">
-//                 <CryptoDetail />
-//               </Route>
-//               <Route exact path="/news">
-//                 <News />
-//               </Route>
-//             </Switch>
-//           </div>
-//         </Layout>
-//       </div>
-//       <div className="footer">
-//         <Typography.Title
-//           level={5}
-//           style={{ color: "white", textAlign: "center" }}
-//         >
-//           CryptoHub <br />
-//           All rights reserved
-//         </Typography.Title>
-//         <Space>
-//           <Link to="/">Home</Link>
-//           <Link to="/exchanges">Exchanges</Link>
-//           <Link to="/news">News</Link>
-//         </Space>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default App;
-
-
 
 import React from 'react';
 import { Switch, Route, Link } from 'react-router-dom';
 import { Layout, Typography, Space } from 'antd';
 import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetail, } from "./components";
 import './App.css';
-
 const App = () => (
   <div className="app">
     <div className="navbar">
@@ -97,9 +32,9 @@ const App = () => (
         </div>
       </Layout>
       <div className="footer">
-        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2021
+        <Typography.Title level={5} style={{ color: 'white', textAlign: 'center' }}>Copyright © 2024 
           <Link to="/">
-            Cryptoverse Inc.
+             CryptoHub Inc.
           </Link> <br />
           All Rights Reserved.
         </Typography.Title>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import millify from 'millify';
+import { Typography,Row,Col,Statistic } from 'antd';
+import { Link } from 'react-router-dom';
 
+const {Title}=Typography;
 const Homepage = () => {
   return (
     <div>
-      <h1>Homepage</h1>
-      bhfczbxncvjslvdb,nc kh.gkvcn zxc/sdh jxflegifl.slc x AD:KSLhfbvxc bfdfskcnsbdihfgw;
+     <>
+     <Title level={2} className='heading'>Global Crypto Status</Title>
+     <Row>
+      <Col span={12}><Statistic title=" Total Cryptocurrencies" value="5"/></Col>
+      <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
+      <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
+      <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
+      <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+     </Row>
+     </>
     </div>
   )
 }

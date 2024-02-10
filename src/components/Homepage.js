b238f8e37b1f1fbcb1fefe38561a536a02129fcb
Tanuja <tanujakhatun292@gmail.com>
2024-02-10 11:10:38 +0530

    project updated

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -25,10 +25,10 @@ const Homepage = () => {
      <Title level={2} className='heading'>Global Crypto Status</Title>
      <Row>
       <Col span={12}><Statistic title=" Total Cryptocurrencies" value={globalStats.total}/></Col>
-      <Col span={12}><Statistic title="Total Exchanges" value="5"/></Col>
-      <Col span={12}><Statistic title="Total Market Cap" value="5"/></Col>
-      <Col span={12}><Statistic title="Total 24h Volume" value="5"/></Col>
-      <Col span={12}><Statistic title="Total Markets" value="5"/></Col>
+      <Col span={12}><Statistic title="Total Exchanges" value={millify(globalStats.totalExchanges)}/></Col>
+      <Col span={12}><Statistic title="Total Market Cap" value={millify(globalStats.totalMarketCap)}/></Col>
+      <Col span={12}><Statistic title="Total 24h Volume" value={millify(globalStats.total24hVolume)}/></Col>
+      <Col span={12}><Statistic title="Total Markets" value={millify(globalStats.totalMarkets)}/></Col>
      </Row>
      </>
     </div>

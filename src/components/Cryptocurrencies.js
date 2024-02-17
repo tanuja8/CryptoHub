4193acde2286f724eae29b9b59c62218cc0f84aa
Tanuja <tanujakhatun292@gmail.com>
2024-02-17 23:17:27 +0530

    project updated

--- src/components/Cryptocurrencies.js
+++ src/components/Cryptocurrencies.js
@@ -12,13 +12,13 @@ const Cryptocurrencies = () => {
   // console.log("Cryptocurrencies component rendered");
   return (
     <>
-    <Row gutters={[32,32]} className='crypto-card-container'>
+    <Row gutter={[32,32]} className='crypto-card-container'>
       {cryptos.map((currency)=>(
-        <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.id}>
-          <Link> to={`/crypto/${currency.id}`}
+        <Col xs={24} sm={12} lg={6} className='crypto-card' key={currency.uuid}>
+          <Link key={currency.uuid} to={`/crypto/${currency.uuid}`}>
           <Card 
             title={`${currency.rank}.${currency.name}`}
-              extra={<img className='crypto-image' src='{currency.iconUrl}'/>}
+              extra={<img className='crypto-image' src={currency.iconUrl}/>}
               hoverable
               >
                 <p>Price:{millify(currency.price)}</p>

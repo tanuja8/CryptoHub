bd665f02712a6fa6d81f280335ecb23e63414515
Tanuja <tanujakhatun292@gmail.com>
2024-02-21 21:04:30 +0530

    project updated

--- src/components/News.js
+++ src/components/News.js
@@ -1,13 +1,19 @@
-import React from 'react'
-import { Select,Typography,Row, Col,Avatar,Card} from 'antd';
-import moment from 'moment';
+import React from "react";
+import { Select, Typography, Row, Col, Avatar, Card } from "antd";
+import moment from "moment";
+import { useGetCryptoNewsQuery } from "../services/cryptoNewsApi";
+import { useGetCryptosQuery } from "../services/cryptoApi";
+import { SmileFilled } from "@ant-design/icons";
 
-const News = () => {
-  return (
-    <div>
-      News
-    </div>
-  )
-}
+const { Text, Title } = Typography;
+const { Option } = Select;
+const News = ({ Smiplified}) => {
+  const { data: cryptoNews } = useGetCryptoNewsQuery({
+    newsCategory: 'Cryptocurrency',
+    count: Smiplified ? 10 : 100,
+  });
+  console.log(cryptoNews);
+  return <div>News</div>;
+};
 
-export default News
+export default News;
